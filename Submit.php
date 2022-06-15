b15cdb0657009862d17a3375750c926a94c987bb
Моисеева София <97868191+Wwesna@users.noreply.github.com>
2022-06-15 13:58:58 +0700

    73

---
+++ Submit.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'Form.php';
+require_once 'Input.php';
+
+	class Submit extends Input
+	{
+		public function __construct()
+		{
+			$this->setAttr('type', 'submit');
+			parent::__construct();
+		}
+
+	}
+    
+?>
